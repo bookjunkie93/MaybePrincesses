c4d6642199a57e9999e3c39b2c30df52bd605967
John Bradley <johnbradley@wr-130-64-94-92.medford.tufts.edu>
2015-02-15 12:53:44 -0500

    Replaced basically everything with my own stuff so I can merge it with the previous version and add the two together

---
+++ Assets/Scripts/Overworld/Cursor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour
+{
+	public GameObject Player;
+
+	private Vector3 pos;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+		pos = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Movement ();
+	}
+
+	private void Movement ()
+	{
+		if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
+		{
+			pos = Vector3.right;
+		}
+		else if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
+		{
+			pos = Vector3.left;
+		}
+		else if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
+		{
+			pos = Vector3.up;
+		}
+		else if (Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow)) 
+		{
+			pos = Vector3.down;
+		}
+
+		transform.position = Player.transform.position + pos;
+	}
+
+	public void Interact ()
+	{
+		Debug.Log ("Yo");
+	}
+}
