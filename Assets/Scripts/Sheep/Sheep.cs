8614c6a99f6c94044af688ead7b9e46f038da31a
jaeteekae <julia.knight@tufts.edu>
2015-02-27 09:14:17 -0500

    added scripts back to sheep minigame

---
+++ Assets/Scripts/Sheep/Sheep.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Sheep : MonoBehaviour {
+//	Vector3 startingPosition;
+	int i = 0;
+	bool call = true;
+
+	// Use this for initialization
+	void Start () {
+//		startingPosition = transform.position;
+	}
+
+	IEnumerator Wait() {
+		yield return new WaitForSeconds(1);
+		call = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (call) {
+			if (i % 4 == 0) {
+				transform.position += Vector3.up * (Time.deltaTime) * 1000;
+				call = false;
+				StartCoroutine (Wait ());
+			} else if (i % 4 == 1) {
+				transform.position += Vector3.left * (Time.deltaTime) * 1000;
+				call = false;
+				StartCoroutine (Wait ());
+			} else if (i % 4 == 2) {
+				transform.position += Vector3.down * (Time.deltaTime) * 1000;
+				call = false;
+				StartCoroutine (Wait ());
+			} else if (i % 4 == 3) {
+				transform.position += Vector3.right * (Time.deltaTime) * 1000;
+				call = false;
+				StartCoroutine (Wait ());
+			}
+			i++;
+		}
+
+	}
+}
