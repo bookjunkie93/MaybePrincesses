8c316e962588e88b9201f2538f67660ec1bfd0e0
jaeteekae <julia.knight@tufts.edu>
2015-03-26 16:29:50 -0400

    added some more buttons for more functionality

--- Assets/Scripts/Sheep/ResetCode.cs
+++ Assets/Scripts/Sheep/ResetCode.cs
@@ -2,14 +2,21 @@
 using System.Collections;
 
 public class ResetCode : MonoBehaviour {
-
+	[SerializeField] Transform slots;
 	// Use this for initialization
 	void Start () {
 	
 	}
 
-	public void ResetCodeGame () {
-		Application.LoadLevel(2);
+	public void EraseCode() {
+		foreach (Transform slotTransform in slots) {
+			GameObject item = slotTransform.GetComponent<Slot1>().item;
+			if (item){
+				Destroy(item);
+				//				Debug.Log(item.name);
+			}
+			
+		}
 	}
 	
 	// Update is called once per frame
