9a337acacde0e513219477f38fc4efd0c58c28a5
John Bradley <johnbradley@wr-130-64-94-139.medford.tufts.edu>
2015-04-22 22:17:55 -0400

    I don't even know

---
+++ Assets/Scripts/Overworld/Follower1.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Follower1 : MonoBehaviour {
+	public static Follower1 instance;
+
+	Transform target; //the enemy's target 
+//	float moveSpeed = 8F; //move speed 
+//	float rotationSpeed = 3F; //speed of turning
+	public float distance;
+//	Quaternion perp = new Quaternion (0f, 0f, 0f, 1f);
+//	static int rerotate;
+	Vector3 lastPosition;
+
+	public Queue prevPositions = new Queue();
+	
+	Transform myTransform; //current transform data of this enemy
+	
+	void Awake() { 
+		instance = this;
+		myTransform = transform; //cache transform data for easy access/preformance 
+	}
+	
+	void Start() { 
+		target = GameObject.FindWithTag("Player").transform; //target the player
+//		rerotate = 0;
+
+		lastPosition = transform.position;
+
+		prevPositions.Enqueue (transform.position);
+		prevPositions.Enqueue (transform.position);
+		prevPositions.Enqueue (transform.position);
+	}
+	
+	void Update () { 
+//		Debug.Log ("UPDATING");
+		//rotate to look at the player 
+		distance =Vector3.Distance(transform.position,target.position);
+
+		if (lastPosition != transform.position) {
+			prevPositions.Enqueue (transform.position);
+		}
+		lastPosition = transform.position;
+		
+		if (distance > 0.75) {
+			myTransform.position = (Vector3) Walking.instance.prevPositions.Dequeue();
+		}
+		
+	}
+}
--- Assets/Scripts/Overworld/Walking.cs
+++ Assets/Scripts/Overworld/Walking.cs
@@ -10,13 +10,15 @@ public class Walking : MonoBehaviour
 	public float dist;
 
 	public Cursor cursor;
-	public GameObject player;
 
 	private float speed;
 	private Vector3 pos;
 	private Transform tr;
 
 	private Animator anim;
+
+	public Queue prevPositions = new Queue();
+	Vector3 lastPosition;
 
 
 	void Awake()
@@ -29,19 +31,25 @@ public class Walking : MonoBehaviour
 		//Here we set the Values from the current position	
 		anim = GetComponent<Animator>();
 
-		pos = GameManagerScript.control.currentPos;
+		pos = GameManagerScript.control.overworldPos;
 		transform.position = pos;
 		tr = transform;
 
+		lastPosition = transform.position;
+		prevPositions.Enqueue (transform.position);
+		prevPositions.Enqueue (transform.position);
 	}
 	
 	void Update () 
 	{
-
 		Movement ();
 		GetSpeed ();
 		Interact ();
-	
+
+		if (lastPosition != transform.position) {
+			prevPositions.Enqueue (transform.position);
+		}
+		lastPosition = transform.position;
 	}
 
 	private void GetSpeed ()
@@ -64,68 +72,29 @@ public class Walking : MonoBehaviour
 		
 		//But we Check if we are at the new Position, before we can add some more
 		//it will prevent to move before you are at your next 'tile'
-
-		if ((Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow)) && tr.position == pos) 
+		if ((Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow)) && tr.position == pos /*&& CheckTarget (Vector3.right)*/) 
 		{
-
-			if (anim.GetInteger("state") != 7) {
-
-				transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x),transform.localScale.y,transform.localScale.z);
-				anim.SetInteger("state", 7);
-			} else {
-				if (CheckTarget (Vector3.right) && !anim.IsInTransition(0))
-				{
-					pos += Vector3.right;
-				}
-
-			}
+			transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x),transform.localScale.y,transform.localScale.z);
+			anim.SetInteger("state", 7);
+			pos += Vector3.right;
 
 		}
-		else if ((Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow)) && tr.position == pos) 
+		else if ((Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow)) && tr.position == pos /*&& CheckTarget (Vector3.left)*/) 
 		{
-
-			if (anim.GetInteger("state") != 5) {
-				transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x),transform.localScale.y,transform.localScale.z);
-				anim.SetInteger("state", 5);
-			} else {
-
-				if (CheckTarget (Vector3.left) && !anim.IsInTransition(0))
-				{
-					pos += Vector3.left;
-				}
-			}
+			transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x),transform.localScale.y,transform.localScale.z);
+			anim.SetInteger("state", 5);
+			pos += Vector3.left;
 		}
-		else if ((Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow)) && tr.position == pos) 
+		else if ((Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow)) && tr.position == pos /*&& CheckTarget (Vector3.up)*/) 
 		{
-			if (anim.GetInteger("state") != 4) {
-				anim.SetInteger("state", 4);
-			} else {
-
-				if (CheckTarget (Vector3.up) && !anim.IsInTransition(0))
-				{
-					pos += Vector3.up;
-				}
-			}
+			anim.SetInteger("state", 4);
+			pos += Vector3.up;
 	
 		}
-		else if ((Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow)) && tr.position == pos)
+		else if ((Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow)) && tr.position == pos /*&& CheckTarget (Vector3.down)*/) 
 		{
-		
-
-			if (anim.GetInteger("state") != 6) {
-				anim.SetInteger("state", 6);
-			} else {
-
-				if (CheckTarget (Vector3.down) && !anim.IsInTransition(0))
-				{
-					pos += Vector3.down;
-				}
-			}
-
-		
-
-
-		
+			anim.SetInteger("state", 6);
+			pos += Vector3.down;
 		} 
 
 		if(!Input.anyKey) {
@@ -141,22 +110,24 @@ public class Walking : MonoBehaviour
 			}
 		}
 
-
+		//Here you will move Towards the new position ...
 		transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
 		
 		
 	}
 
-	private bool CheckTarget (Vector3 dir3)
+	private bool CheckTarget (Vector3 dir)
 	{
-		Vector2 dir = dir3;
 		RaycastHit2D obst;
 		obst = Physics2D.Raycast (transform.position, dir, 1F);
-
-		if (obst.collider != null && !obst.collider.isTrigger) {
-			return false;
-		} else {
+		dist = obst.distance;
+		if (dist > 0.6F)
+		{
 			return true;
+		}
+		else
+		{
+			return false;
 		}
 	}
 
@@ -166,12 +137,6 @@ public class Walking : MonoBehaviour
 		{
 			cursor.Interact ();
 		}
-	}
-
-	public void Door2Door (Vector3 destination)
-	{
-		transform.position = destination;
-		pos = destination;
 	}
 }
 
